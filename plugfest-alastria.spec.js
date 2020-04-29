c84746d10bc1a7a2dc89440acafd1d215fd610f6
Nicolas Manero <nicolas.manero@4tic.com>
2020-04-29 08:14:41 +0200

    #51 Iteramos por todos los valores de dids y verificamos que existen

--- plugfest-alastria.spec.js
+++ plugfest-alastria.spec.js
@@ -1,10 +1,22 @@
 var describe = require('mocha').describe
+var expect = require('chai').expect;
+var should = require('chai').should();
 const vendors = require('./vendors');
 
 describe('Plugfest Alastria 2020', () => {
     vendors.forEach(vendor => {
       describe(vendor.name, () => {
-
+        
+        vendor.dids.forEach(didObject => {
+          var keysDIDs = Object.keys(didObject);
+          
+          keysDIDs.forEach(keyDID => {
+            it('did should exist', function () {
+              console.log(didObject[keyDID]);
+              should.exist(didObject[keyDID]);
+            });
+          });
+        })
       });
     });
 });
