a3e7b33944861ea0a5dc732733ab69a770c6398e
Nicolas Manero <nicolas.manero@4tic.com>
2020-04-29 08:28:51 +0200

    #51 Usamos los validarores añadidos

--- plugfest-alastria.spec.js
+++ plugfest-alastria.spec.js
@@ -1,7 +1,7 @@
 var describe = require('mocha').describe
 var expect = require('chai').expect;
-var should = require('chai').should();
 const vendors = require('./vendors');
+const validators = require('./validators')
 
 describe('Plugfest Alastria 2020', () => {
     vendors.forEach(vendor => {
@@ -12,8 +12,7 @@ describe('Plugfest Alastria 2020', () => {
           
           keysDIDs.forEach(keyDID => {
             it('did should exist', function () {
-              console.log(didObject[keyDID]);
-              should.exist(didObject[keyDID]);
+              expect(validators.did.shouldExist(didObject[keyDID])).to.be.true;
             });
           });
         })
