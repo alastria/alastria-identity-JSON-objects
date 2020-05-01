841854c812c121b1400a809ae3d17de12f8970eb
Nicolas Manero <nicolas.manero@4tic.com>
2020-05-01 20:16:21 +0200

    #51 Added custom error messages in tests in case they fail

--- plugfest-alastria.spec.js
+++ plugfest-alastria.spec.js
@@ -13,23 +13,23 @@ describe('Plugfest Alastria 2020', () => {
           keysDIDs.forEach(keyDID => {
             describe("Testing: " + didObject[keyDID], () => {
               it('did should exist', function () {
-                expect(validators.did.shouldExist(didObject[keyDID])).to.be.true;
+                expect(validators.did.shouldExist(didObject[keyDID]), "Did does not exist").to.be.true;
               });
 
               it('did should start with did:', function() {
-                expect(validators.did.shouldStartWithDID(didObject[keyDID])).to.be.true;
+                expect(validators.did.shouldStartWithDID(didObject[keyDID]), "Did does not start with characters 'did'").to.be.true;
               });
 
               it('did should start "ala" as identifier', function() {
-                expect(validators.did.shouldHaveAlaAsIdentifier(didObject[keyDID])).to.be.true;
+                expect(validators.did.shouldHaveAlaAsIdentifier(didObject[keyDID]), "Did should have 'ala' as identifier").to.be.true;
               });
 
               it('did should start "quor" or "fabr" as network', function() {
-                expect(validators.did.shouldHaveQuorOrFabrAsNetwork(didObject[keyDID])).to.be.true;
+                expect(validators.did.shouldHaveQuorOrFabrAsNetwork(didObject[keyDID]), "Did should have 'quor' or 'fabr' as network").to.be.true;
               });
 
               it('did should have hex in proxyAddress', function() {
-                expect(validators.did.shouldProxyAddressBeHexadecimal(didObject[keyDID])).to.be.true;
+                expect(validators.did.shouldProxyAddressBeHexadecimal(didObject[keyDID]), "Did should have a valid hex value as proxy address").to.be.true;
               });
             });
           });
