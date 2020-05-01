2e62af162edb482c92efa10981dcf31fcaf04853
Nicolas Manero <nicolas.manero@4tic.com>
2020-05-01 18:33:24 +0200

    #52 Exporting credentials validator

--- validators/credentials/index.js
+++ validators/credentials/index.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const credentialValidadorFactory = {
+    "shouldExist": shouldExist
+}
+
+function shouldExist(credential) {
+    return credential != null && credential != undefined;
+}
+
+module.exports = credentialValidadorFactory;
