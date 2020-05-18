63d69b2be4b67592a8a00172966cc7f2be66b56f
carlosalvarezrivas <carlosalvarez@lacometa.com>
2020-05-18 15:48:28 +0200

    Add 2. Test Presentations

--- vendors/alastria/index.js
+++ vendors/alastria/index.js
@@ -2,11 +2,13 @@
 
 const dids = require('./dids');
 const credentials = require('./credentials');
-const tokens = require('./tokens')
+const tokens = require('./tokens');
+const presentations = require('./presentations');
 
 module.exports = {
-  name: 'Alastria',
-  dids: [...dids],
-  credentials: [...credentials],
-  tokens: [...tokens]
+    name: 'Alastria',
+    dids: [...dids],
+    credentials: [...credentials],
+    tokens: [...tokens],
+    presentations: [...presentations]
 };
