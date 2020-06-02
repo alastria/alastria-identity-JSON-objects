2f8c84311ed50434645360cf15bbd995f54f7df9
Nicolas Manero <nicolas.manero@4tic.com>
2020-06-02 16:08:44 +0200

    #59 Refactoring for extracting presentation requests tests into a new file

--- plugfest-alastria.spec.js
+++ plugfest-alastria.spec.js
@@ -5,7 +5,6 @@ const validators = require('./validators')
 var chai = require('chai');
 chai.use(require('chai-json-schema'));
 const jwt = require('jsonwebtoken');
-var presentationRequestSchema = require('./validators/presentationRequests/presentationRequest-json-schema.json');
 var alastriaIdCreationSchema = require('./validators/alastriaIdCreations/alastriaIdCreation-json-schema.json');
 var { tests } = require('./tests')
 
@@ -72,23 +71,7 @@ describe('Plugfest Alastria 2020', () => {
                 var presentationRequestAsBase64 = presentationRequestObject[keyPresentationRequest];
 
                 describe("Testing Presentation Request: " + presentationRequestAsBase64, () => {
-                    it('Presentation Request should exist', function () {
-                        expect(validators.presentationRequests.shouldExist(presentationRequestAsBase64), "Presentation Request should exist").to.be.true;
-                    });
-
-                    it('Token should be a valid JWT structure', function () {
-                        expect(validators.presentationRequests.shouldHaveAValidJWTStructureWithThreeSegmentsSeparatedByDots(presentationRequestAsBase64), "It should follow the structure string.string.string").to.be.true;
-                    });
-
-                    var decodedPresentationRequest = jwt.decode(presentationRequestAsBase64, {complete: true});
-
-                    it('Validate schema of the Presentation Request ' + decodedPresentationRequest, () => {
-                        expect(decodedPresentationRequest).to.be.jsonSchema(presentationRequestSchema);
-                    });
-
-                    it('Property CBU of the decoded payload should be a valid URL', function () {
-                        expect(validators.presentationRequests.shouldPropertyCBUInDecodedPayloadBeAValidURL(decodedPresentationRequest), "Property 'cbu' inside decoded payload should be a valid URL").to.be.true;
-                    });
+                    tests.presentationRequests.validatePresentationRequest(presentationRequestAsBase64);
                 });
             });
         });
--- tests/index.js
+++ tests/index.js
@@ -3,12 +3,14 @@ const credentials = require('./credentials')
 const tokens = require('./tokens')
 const sessions = require('./sessions')
 const presentations = require('./presentations')
+const presentationRequests = require('./presentationRequests')
 const tests = {
     "dids": dids,
     "credentials": credentials,
     "tokens": tokens,
     "sessions": sessions,
-    "presentations": presentations
+    "presentations": presentations,
+    "presentationRequests": presentationRequests
 }
 
 module.exports.tests = tests;
